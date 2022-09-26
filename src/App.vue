a0d02d5bdf87aeae0fdf7ef66fc3e25ebbc84579
John <jcastro@crnova.com>
2022-09-25 18:26:09 -0600

    197. CSS Animations with the Transition Component

--- src/App.vue
+++ src/App.vue
@@ -90,39 +90,41 @@ button:active {
 
 .animate {
   /* transform: translateX(-50px); */
-  animation: slide-fade 0.3s ease-out forwards;
+  animation: slide-scale 0.3s ease-out forwards;
   transform: translateY(-30px);
 }
 
-.v-enter-from {
+/* .v-enter-from {
   opacity: 0;
   transform: translateY(-30px);
-}
+} */
 
 .v-enter-active {
-  transition: all 0.6s ease-out;
+  /* transition: all 0.6s ease-out; */
+  animation: slide-scale 0.6s ease-in;
 }
 
-.v-enter-to {
+/* .v-enter-to {
   opacity: 1;
   transform: translateY(0);
-}
-
+} */
+/* 
 .v-leave-from {
   opacity: 1;
   transform: translateY(0px);
-}
+} */
 
 .v-leave-active {
-  transition: all 0.6s ease-in;
+  /* transition: all 0.6s ease-in; */
+  animation: slide-scale 0.6s ease-out;
 }
 
-.v-leave-to {
+/* .v-leave-to {
   opacity: 0;
   transform: translateY(30px);
-}
+} */
 
-@keyframes slide-fade {
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   }
